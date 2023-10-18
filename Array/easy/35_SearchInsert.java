77c9991ef43a772fc6ef6ba8ff5bbfb05a135241
z5409906 <z5409906@ad.unsw.edu.au>
2023-10-18 17:37:10 +1100

    update array questions

---
+++ Array/easy/35_SearchInsert.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int mid = 0;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            mid = left + ((right - left) >> 1);
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+            if (left == right){
+                if (target <= nums[left]){
+                    return left;
+                }else{
+                    return left + 1;
+                }
+            }
+        }
+        if (left>0){
+            return left;
+        }else{
+            return 0;
+        }
+
+    }
+
+}
